27dc825a738f6df0641b78496fb5be4b16ab988f
Ryan3773 <ryanalexander@mail.weber.edu>
2024-10-09 23:42:10 -0600

    M3 Completed!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  if (req.session.test === undefined) {
+    console.log("Session key test not set");
+    req.session.test = "Putting data in a session variable";
+    req.session.save(function(err) {
+      if (err) {
+        throw err;
+      }
+      console.log("Session variable set test: " + req.session.test);
+    });
+  } else {
+    console.log("Session variable set test: " + req.session.test);
+  }
+
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
